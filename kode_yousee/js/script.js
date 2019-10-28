10bcd4a6884ae975c25e3a2266b74476b4918b69
Thomas Gimm Hvergel <48520858+ThomasGimm@users.noreply.github.com>
2019-10-28 14:31:57 +0100

    Add files via upload

---
+++ kode_yousee/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+  $("#button1").click(function () {
+    alert("Forkert")
+  });
+
+  $("#button2").click(function () {
+    alert("Forkert")
+  });
+
+  $("#button3").click(function () {
+    alert("Rigtigt")
+  });
+
+  var options = {
+    videoId: 'FNkfxpurea8',
+    start: 3
+  };
+  $('#wrapper').tubular(options);
+  // f-UGhWj1xww cool sepia hd
+  // 49SKbS7Xwf4 beautiful barn sepia
+
+});
